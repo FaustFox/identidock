06e8276c7d5cdc284132b96d542b423aeb72606f
FaustFox <faustffox@gmail.com>
2020-06-24 17:27:29 +0300

    add test var

--- cmd.sh
+++ cmd.sh
@@ -5,6 +5,9 @@ set -e
 if [ "$ENV" = 'DEV' ]; then
 	echo "Running DEV server"
 	exec python "identidock.py"
+elif [ "$ENV" = 'UNIT' ]; then
+	echo "Running UNIT tests"
+	exec python "test.py"
 else
 	echo "Running PROD server"
 	exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
